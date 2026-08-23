af98fdcdf3cf5ac57428c744380930f9c2176325
Riadh Ben Hamouda <33267741+RiadhInfo@users.noreply.github.com>
2026-08-23 17:18:06 +0100

    initial commit

---
+++ backend/src/main/java/com/tn/dev/ApiBackendApplication.java
@@ -0,0 +1,10 @@
+package com.tn.dev;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBackendApplication.class, args);
+    }
+}
