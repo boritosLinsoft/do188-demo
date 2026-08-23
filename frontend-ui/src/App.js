f5e34ab7949b47e7386fcd05e47f4facfa0ab40e
Riadh Ben Hamouda <33267741+RiadhInfo@users.noreply.github.com>
2026-08-23 23:04:15 +0100

    modify appJs files

--- frontend-ui/src/App.js
+++ frontend-ui/src/App.js
@@ -35,7 +35,7 @@ function App() {
             alt="Red Hat Logo" 
             style={{ height: '40px' }} 
           />
-          <h2 style={{ color: 'white', margin: 0, fontSize: '1.4rem', fontWeight: '500' }}>Cloud-Native App</h2>
+          <h2 style={{ color: 'white', margin: 0, fontSize: '1.4rem', fontWeight: '500' }}>ANCE App</h2>
         </div>
         
         {/* Le Lien (Bouton) elli yhezzek lil Tâches */}
